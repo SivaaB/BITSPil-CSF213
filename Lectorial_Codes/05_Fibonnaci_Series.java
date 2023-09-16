f9245d336bc424a9ff082a3ca7d02488c5f8b924
Sivaa B <mmailsivaa@gmail.com>
2023-09-16 23:13:54 +0400

    lectorial codes

---
+++ Lectorial_Codes/05_Fibonnaci_Series.java
@@ -0,0 +1,25 @@
+import java.util.Scanner; 
+
+public class Main
+{ 
+    public static void main (String[] args)
+    { 
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.println ("Term Number: "); 
+        int n = scanner.nextInt(); 
+        
+        int prev = 0; 
+        int current = 1; 
+        int next; 
+
+        for (int i = 2; i <= n; i++) 
+        { 
+            next = prev + current; 
+            prev = current; 
+            current = next; 
+        }
+
+        System.out.println ("The nth term is: " + current); 
+    }
+}
