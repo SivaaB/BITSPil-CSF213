ec6c6368ef98c204f034be63b87c50c3349a282a
Sivaa Balamurugan <74814237+SivaaB@users.noreply.github.com>
2023-09-24 18:06:42 +0400

    Update GameLauncher.java

--- Lab/Week - 3/2/GameLauncher.java
+++ Lab/Week - 3/2/GameLauncher.java
@@ -1,63 +1,60 @@
 import java.util.Scanner;
 import java.util.Random;
 
-public class GameLauncher
-{
-    public static void main (String[] args)
-    {
-        System.out.println ("Guessing Games - Guess a Number between 0 to 9!");
-
-        GuessGame game = new GuessGame();
-        game.startGame();
+public class GameLauncher {
+    public static void main(String[] args) {
+        System.out.println("Guessing Games - Guess a Number between 0 to 9!");
 
         Player p1 = new Player();
         Player p2 = new Player();
         Player p3 = new Player();
+
+        GuessGame game = new GuessGame(p1, p2, p3);
+        game.startGame();
     }
 }
 
-class GuessGame
-{
-    static int p1;
-    static int p2;
-    static int p3;
+class GuessGame {
+    private Player p1;
+    private Player p2;
+    private Player p3;
 
-    public static void startGame()
-    {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println ("User-1: ");
-        int p1 = scanner.nextInt();
-        System.out.println ("User-2: ");
-        int p2 = scanner.nextInt();
-        System.out.println ("User-3: ");
-        int p3 = scanner.nextInt();
-
-        int number = Player.guess();
-        System.out.println ("The random number is: " + number);
-
-        System.out.println ("Winners are: ");
-        if (number == p1)
-        {
-            System.out.println ("Winner-1");
-        }
-        if (number == p2)
-        {
-            System.out.println ("Winnner-2");
-        }
-        if (number == p3)
-        {
-            System.out.println ("Winnner-3");
-        }
+    public GuessGame(Player player1, Player player2, Player player3) {
+        this.p1 = player1;
+        this.p2 = player2;
+        this.p3 = player3;
     }
-}
 
-class Player
-{
-    public static int guess ()
-    {
+    public void startGame() {
+        int p1Guess = p1.guess();
+        int p2Guess = p2.guess();
+        int p3Guess = p3.guess();
+
         Random random = new Random();
         int number = random.nextInt(10);
-        return number;
+
+        System.out.println("The random number is: " + number);
+
+        System.out.println("Winners are: ");
+        if (number == p1Guess) {
+            System.out.println("Winner-1");
+        }
+        if (number == p2Guess) {
+            System.out.println("Winner-2");
+        }
+        if (number == p3Guess) {
+            System.out.println("Winner-3");
+        }
+    }
+}
+
+class Player {
+    private int guess;
+    public int guess() 
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your guess: ");
+        guess = scanner.nextInt();
+        return guess;
     }
 }
