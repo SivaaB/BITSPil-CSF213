c158b52569edf944b97ecac3b051b659ca1839e0
Sivaa B <mmailsivaa@gmail.com>
2023-10-10 10:30:44 +0400

    need to edit

---
+++ Lab/Week - 7/1/Q1.java
@@ -0,0 +1,29 @@
+class Car 
+{ 
+    String vehicle_id; 
+    float [] Cost; // stores base, exercise duty and sales tax for each car. 
+    float total; 
+    float Grand_Total; 
+
+    calc_total(total, base, exercise_duty, sales_tax)
+    { 
+        total = base + exercise_duty + sales_tax; 
+        return total; 
+    }
+
+    
+}
+
+public class Q1
+{ 
+    public static void main (String[] args)
+    { 
+        int total = 0; 
+
+        System.out.println ("Enter name of the vehicle: "); 
+        System
+
+        total = calc_grand_total (int total, float Cost[0], float Cost[1], float Cost[2]); 
+        Grand_Total = calc_grand_total(int total); 
+    }
+}
